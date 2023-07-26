5cf84886079b369a21ed904625de2672d93347e7
«dima» <dima-titov-93@mail.ru>
2023-07-26 19:17:16 +0300

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class Person {
+    public String fullName;
+    public Gender gender;
+
+    public Person(String fullName, Gender gender) {
+        this.fullName = fullName;
+        this.gender = gender;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+}
+
